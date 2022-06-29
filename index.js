b64468b5279bfcefe3a70312cbee892b4eb974da
gepiko <aoriaira@gmail.com>
2022-06-29 15:43:12 +0300

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import multer from 'multer'
+import cors from 'cors'
 
 import mongoose from 'mongoose'
 
@@ -28,6 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
