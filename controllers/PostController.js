a14da391e14a93a2e0aab83b5247b07b4160e2c9
gepiko <aoriaira@gmail.com>
2022-07-06 15:38:37 +0300

    small fix with tags and user avatar

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -59,7 +59,7 @@ export const getOne = async (req, res) => {
 
         res.json(doc)
       },
-    )
+    ).populate('user')
   } catch (err) {
     console.log(err)
     res.status(500).json({
@@ -109,7 +109,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     })
 
@@ -137,7 +137,7 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userId,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
       },
     )
 
